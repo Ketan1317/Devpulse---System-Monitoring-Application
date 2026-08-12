57ce45c8d003f6a4482bba586067ae3123d5cb5e
Ketan1317 <goyalketan1317@gmail.com>
2026-08-13 00:50:01 +0530

    prepare frontend for deployment

--- Devpulse/src/main/java/com/Project/Devpulse/Config/CorsConfig.java
+++ Devpulse/src/main/java/com/Project/Devpulse/Config/CorsConfig.java
@@ -1,12 +1,12 @@
 package com.Project.Devpulse.Config;   // Recommended: lowercase 'config'
 
+import java.util.List;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-
-import java.util.List;
 
 @Configuration
 public class CorsConfig {
@@ -17,8 +17,7 @@ public class CorsConfig {
 
         // Allowed origins
         configuration.setAllowedOrigins(List.of(
-                "http://localhost:5173",
-                "http://127.0.0.1:5173"
+                "http://localhost:5173"
         ));
 
         // Allowed methods
